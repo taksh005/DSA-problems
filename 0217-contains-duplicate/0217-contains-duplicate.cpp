01f1cdbab92842bb70c59ffbff9b784efc96e3b6
taksh005 <137983458+taksh005@users.noreply.github.com>
2026-03-21 22:19:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         unordered_map<int,int> count;
         for(int i =0;i<n;i++){
+            if(count[nums[i]] == 1) return true;
             count[nums[i]] += 1;
-            if(count[nums[i]] > 1) return true;
         }
         return false;
     }
