dddae632c99d3558cc2e78cf9cc524c3e623e9e2
taksh005 <137983458+taksh005@users.noreply.github.com>
2026-03-21 22:27:03 +0530

    Time: 28 ms (78.12%), Space: 71 MB (88.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,8 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        for(int i =1;i<n-1;i+2){
-            if(nums[i] == nums[i-1] || nums[i] == nums[i+1]) return true;
+        for(int i =1;i<n;i++){
+            if(nums[i] == nums[i-1]) return true;
         }
         return false;
     }
