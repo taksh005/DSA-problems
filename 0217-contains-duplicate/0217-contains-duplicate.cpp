8d1566dd11c5e1cb695a746be9dc8909a97241a9
taksh005 <137983458+taksh005@users.noreply.github.com>
2026-03-21 22:29:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i =1;i<n;i++){
-            if(nums[i] == nums[i-1]) return true;
+        unordered_set<int> visited;
+        for(int i : nums){
+            if(visited.count(i)) return true;
+            visited.insert(i);
         }
         return false;
     }
