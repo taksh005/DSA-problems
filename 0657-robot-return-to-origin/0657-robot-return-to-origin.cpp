e0c636ee3b714054d1bc213fded8374757e0256a
taksh005 <137983458+taksh005@users.noreply.github.com>
2026-04-05 12:32:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -3,6 +3,7 @@ public:
     bool judgeCircle(string moves) {
         unordered_map<char,int> mapping = {{'R',0},{'L',0},{'U',0},{'D',0}};
         for(char c : moves) mapping[c]++;
-        return mapping['R']==mapping['L'] && mapping['U']== mapping['D'];
+        if(mapping['R']!=mapping['L'] || mapping['U']!= mapping['D']) return false;
+        return true;
     }
 };
