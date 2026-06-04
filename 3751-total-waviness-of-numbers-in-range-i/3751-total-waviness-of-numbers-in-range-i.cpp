208b68d695897ab5b361e69fbfaf6013d13240bb
taksh005 <137983458+taksh005@users.noreply.github.com>
2026-06-04 18:57:33 +0530

    Time: 47 ms (61.94%), Space: 9.5 MB (59.7%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,24 +1,13 @@
 class Solution {
-private:
-    void digits_vector(int x,vector<int>& digits){
-        if(x == 0) return;
-        digits.push_back(x%10);
-        return digits_vector(x/10,digits);
-    }
 public:
     int totalWaviness(int num1, int num2) {
         int ans = 0;
-        for(int j = num1; j <= num2; j++){
-            vector<int> digits;
-            digits_vector(j,digits);
-            if(digits.size() < 3) {
-                continue;
+        for(int i = num1;i<=num2;i++){
+            string digits = to_string(i);
+            if(digits.size()<3) continue;
+            for(int j = 1;j<digits.size()-1;j++){
+                if((digits[j] < digits[j+1] && digits[j] < digits[j-1]) || (digits[j] > digits[j+1] && digits[j] > digits[j-1])) ans++;
             }
-            for(int i = 1; i< digits.size() - 1;i++){
-                if((digits[i] > digits[i+1] && digits[i] > digits[i-1]) || (digits[i] < digits[i+1] && digits[i] < digits[i-1])){
-                    ans++;
-                }
-            } 
         }
         return ans;
     }
